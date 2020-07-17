5fec9eb23fc6ee92c9f8d64b1dfab8d8f923954b
d_fei <xpf14561456@163.com>
2020-07-17 21:07:15 +0800

    测试入口

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,29 @@
-import com.train.beans.User;
-import com.train.dao.UserDao;
-
 public class App {
     public static void main(String[] args) {
-        UserDao userDao = new UserDao();
-        User user = userDao.getUser(2);
-        System.out.println(user.toString());
+        /*FormDao formDao = new FormDao();
+        Form form = new Form();
+        form.setId(5);
+        form.setDepartment("部");
+        form.setUser("麻脸");
+        form.setDetailId(3);
+        form.setDate("20201212");
+        HashMap<Integer, ArrayList<Details>> map = new HashMap<Integer, ArrayList<Details>>();
+        Details details1 = new Details();
+        Details details2 = new Details();
+        details1.setDetailId(form.getDetailId());
+        details1.setId(6);
+        details1.setName("测试1");
+        details1.setSpec("haha");
+        details2.setDetailId(form.getDetailId());
+        details2.setId(7);
+        details2.setName("测试二");
+        details2.setSpec("haha");
+        ArrayList<Details> details = new ArrayList<Details>();
+        details.add(details1);
+        details.add(details2);
+        map.put(form.getDetailId(), details);
+        form.setDetail(map);
+        int result = formDao.updateForm(form);
+        System.out.println(result);*/
     }
 }
