2ee612c822138d38a58622af7db45755a8b8696a
d_fei <xpf14561456@163.com>
2020-07-17 20:54:44 +0800

    将details明细类型修改为hashmap后，更改了与之相关的存取操作

--- src/main/java/com/train/dao/DetailsDao.java
+++ src/main/java/com/train/dao/DetailsDao.java
@@ -4,6 +4,8 @@ import com.train.beans.Details;
 import com.train.common.MybatisUtil;
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class DetailsDao {
@@ -20,6 +22,16 @@ public class DetailsDao {
     }
 
     /**
+     * 根据hashmap增加表单中所有明细
+     */
+    public void addDetails(HashMap hashMap,int detailId) {
+        int result = 0;
+        List<Details> details = (List<Details>) hashMap.get(detailId);
+        for (Details details1 : details) {
+            addDetail(details1);
+        }
+    }
+    /**
      * 根据id删除
      */
     public int deleteDetail(int id) {
@@ -29,6 +41,18 @@ public class DetailsDao {
         sqlSession.commit();
         sqlSession.close();
         return result;
+    }
+    /**
+     * 根据detailId删除数据
+     * @param id---穿过来的不是detailId
+     */
+    public void deleteDetails(int id) {
+        Details details = null;
+        details = getDetail(id);
+        SqlSession sqlSession = MybatisUtil.getConn();
+        sqlSession.delete("deleteDetails", details.getDetailId());
+        sqlSession.commit();
+        sqlSession.close();
     }
 
     /**
@@ -41,6 +65,16 @@ public class DetailsDao {
         sqlSession.commit();
         sqlSession.close();
         return result;
+    }
+
+    /**
+     * 联动form修改而修改
+     */
+    public void updateDetails(int detailId,HashMap map) {
+        ArrayList<Details> details = (ArrayList<Details>) map.get(detailId);
+        for (Details detaail : details) {
+            updateDetail(detaail);
+        }
     }
 
     /**
@@ -58,12 +92,14 @@ public class DetailsDao {
     /**
      * 根据detailId查询，返回所有detailId相同的数据，返回一个list
      */
-    public List<Details> getAllDetails(int detailId) {
+    public ArrayList<Details> getAllDetails(int detailId) {
         List<Details> details = null;
         SqlSession sqlSession = MybatisUtil.getConn();
         details = sqlSession.selectList("getAllDetails", detailId);
+        ArrayList<Details> details1 = new ArrayList<Details>();
+        details1.addAll(details);
         sqlSession.commit();
         sqlSession.close();
-        return details;
+        return details1;
     }
 }
