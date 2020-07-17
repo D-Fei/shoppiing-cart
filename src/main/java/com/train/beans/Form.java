06219ff9cc71d6843b14fcbf15131376cbf1f012
d_fei <xpf14561456@163.com>
2020-07-17 18:55:03 +0800

    小修改，增加detailId

---
+++ src/main/java/com/train/beans/Form.java
@@ -0,0 +1,61 @@
+package com.train.beans;
+
+public class Form {
+    private int id; ///表单唯一标识
+    private String department;  //申请部门
+    private String user;  //申请人
+//    private List<Details> detail; //明细
+    private int detailId;
+    private String date; //申请日期
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int getDetailId() {
+        return detailId;
+    }
+
+    public void setDetailId(int detailId) {
+        this.detailId = detailId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Form{" +
+                "id=" + id +
+                ", department='" + department + '\'' +
+                ", user='" + user + '\'' +
+                ", detailId=" + detailId +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
